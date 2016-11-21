d5f41649c88997aed0cad260d5b4c924b0344e79
Coder <coderdba@users.noreply.github.com>
2016-11-21 15:26:17 +0530

    Update json-split1.sh

--- json-split1.sh
+++ json-split1.sh
@@ -6,4 +6,8 @@
  
  Password vault account query output splitter:
  cat res*file | sed -e 's/[{}]/''/g' | sed 's/,"Key/|"Key/g' | awk '{n=split($0,a,"|"); for (i=1; i<=n; i++) print a[i]}'
+ cat res*file | sed -e 's/[{}]/''/g' | sed 's/,"Key/|"Key/g' | sed 's/,"accounts"/\|"accounts"/g' | awk '{n=split($0,a,"|"); for (i=1; i<=n; i++) print a[i]}' | sed 's/"//g'| sed 's/\[//g' | sed 's/\]//g'
+ cat res*file | sed -e 's/[{}]/''/g' | sed 's/,"Key/|"Key/g' |  sed 's/"//g' | sed 's/\,accounts/\|/g' | sed 's/:\[AccountID/AccountID/g' | sed 's/\,Internal/\|Internal/g' | sed 's/\,Properties:\[/\|/g' | awk '{n=split($0,a,"|"); for (i=1; i<=n; i++) print a[i]}'
+ cat res*file | sed -e 's/[{}]/''/g' | sed 's/,"Key/|"Key/g' |  sed 's/"//g' | sed 's/\,accounts/\|/g' | sed 's/:\[AccountID/AccountID/g' | sed 's/\,InternalProperties:\[/\|/g' | sed 's/\,Properties:\[/\|/g' | awk '{n=split($0,a,"|"); for (i=1; i<=n; i++) print a[i]}' | sed 's/\]//g'
+
  
