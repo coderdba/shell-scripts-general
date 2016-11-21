d843f8e232afa4a088657a142063ea9aca095491
Coder <coderdba@users.noreply.github.com>
2016-11-21 15:07:11 +0530

    Update json-split1.sh

--- json-split1.sh
+++ json-split1.sh
@@ -4,3 +4,6 @@
  Same code with a filter k="text" to show only fields with name "text" (but this filter is not working)
  cat filename.txt | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'
  
+ Password vault account query output splitter:
+ cat res*file | sed -e 's/[{}]/''/g' | sed 's/,"Key/|"Key/g' | awk '{n=split($0,a,"|"); for (i=1; i<=n; i++) print a[i]}'
+ 
